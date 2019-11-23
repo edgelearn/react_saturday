0c5c4cf1b6d07ae6ec8dc482d269f46b7b5c4cdc
AdrienneW <57571245+AdrienneWarner@users.noreply.github.com>
2019-11-23 15:53:44 -0700

    1) added calc buttons 2) added calc events 3) added output display 4) added input display 5) added evaluation functioning

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -3,69 +3,82 @@ import Button from '@material-ui/core/Button';
 
 export const Calculator = () => {
     const [calc, setCalc] = useState({
-        input: 'input',
-        output: 'output',
+        input: '',
+        output: 'no output',
     })
 
     return (
         <div>
             <div>
-            <Button variant="contained" value="-" color="secondary">
+              <p>{calc.input ? calc.input : 0}</p>
+              <p>{calc.output}</p>
+            </div>
+            <div>
+            <Button onClick={() => setCalc({...calc, input: calc.input + '-' })} variant="contained" value="-" color="secondary">
               -
             </Button>
-            <Button variant="contained" value="+" color="secondary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '+' })} variant="contained" value="+" color="secondary">
               +
             </Button>
-            <Button variant="contained" value="*" color="secondary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '*' })} variant="contained" value="*" color="secondary">
               *
             </Button>
-            <Button variant="contained" value="/" color="secondary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '/' })} variant="contained" value="/" color="secondary">
               /
+            </Button>
+            <Button onClick={() => setCalc({...calc, input: calc.input + '(' })} variant="contained" value="/" color="secondary">
+              (
+            </Button>
+            <Button onClick={() => setCalc({...calc, input: calc.input + ')' })} variant="contained" value="/" color="secondary">
+              )
             </Button>
             </div>
             <div>
-            <Button variant="contained" value="=" color="grey">
+            <Button onClick={() => setCalc({...calc, output: eval(calc.input)})} variant="contained" value="=" color="grey">
               =
             </Button>
-            <Button variant="contained" value="" color="grey">
+            <Button onClick={() => setCalc({...calc, input: '' , output: ''})} variant="contained" value="" color="grey">
               Clear
+            </Button>
+            <Button variant="contained" value="" color="grey">
+              Back
             </Button>
             </div>
             <br/>
-            <Button variant="contained" value="1" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '1' })} variant="contained" value="1" color="primary">
               1
             </Button>
-            <Button variant="contained" value="2" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '2' })} variant="contained" value="2" color="primary">
               2
             </Button>
-            <Button variant="contained" value="3" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '3' })} variant="contained" value="3" color="primary">
               3
             </Button>
             <br/>
-            <Button variant="contained" value="4" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '4' })} variant="contained" value="4" color="primary">
               4
             </Button>
-            <Button variant="contained" value="5" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '5' })} variant="contained" value="5" color="primary">
               5
             </Button>
-            <Button variant="contained" value="6" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '6' })} variant="contained" value="6" color="primary">
               6
             </Button>
             <br/>
-            <Button variant="contained" value="7" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '7' })} variant="contained" value="7" color="primary">
               7
             </Button>
-            <Button variant="contained" value="8" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '8' })} variant="contained" value="8" color="primary">
               8
             </Button>
-            <Button variant="contained" value="9" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '9' })} variant="contained" value="9" color="primary">
               9
             </Button>
             <br/>
-            <Button variant="contained" value="10" color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '0' })} variant="contained" value="10" color="primary">
               0
             </Button>
-            <Button variant="contained" value="." color="primary">
+            <Button onClick={() => setCalc({...calc, input: calc.input + '.' })} variant="contained" value="." color="primary">
               .
             </Button>
         </div>
